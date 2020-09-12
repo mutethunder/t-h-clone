207c4e67b62a3ea847cc146ef5975027c615c670
Dennis Krinitsyn <69056069+mutethunder@users.noreply.github.com>
2020-09-12 14:33:52 -0400

    Write JSX for List of Watches portion of the webpage

--- src/Components/Watches/watches.js
+++ src/Components/Watches/watches.js
@@ -1,29 +1,15 @@
-class Watch {
-  constructor(src, title, price) {
-    this.src = src;
-    this.price = price;
-    this.title = title;
-  }
+import React from 'react';
+
+class Watch extends React.Component {
+    render(){
+        return (
+            <div className="Watch">
+                <div className="image-container">
+                    <img src={this.props.watch.src} alt="" />
+                </div>
+                <h2>{this.props.watch.title}</h2>
+                <h3>{this.props.watch.price}</h3>
+            </div>
+        )
+    }
 }
-
-const watch1 = new Watch("./JLC 1.jpg", "JLC Reverso", "$9,355 USD");
-const watch2 = new Watch("./Omega 1.jpg", "Omega Constellation", "$1,995 USD");
-const watch3 = new Watch("./Omega 2.jpg", "Omega", "$1,495 USD");
-const watch4 = new Watch("./Omega 3.jpg","Omega Speedmaster 145.022","$6,195 USD");
-const watch5 = new Watch("./Rolex 1.jpg","Rolex Datejust Ref. 1600","$4,295 USD");
-const watch6 = new Watch("./Rolex 2.jpg", "Rolex Air-King", "$3,125 USD");
-const watch7 = new Watch("./Rolex 3.jpg","Rolex Datejust Ref. 1601","$7,750 USD");
-const watch8 = new Watch("./Rolex 4.jpg","Rolex Datejust Ref. 1601","$4,095 USD");
-
-const watchArray = [
-  watch1,
-  watch2,
-  watch3,
-  watch4,
-  watch5,
-  watch6,
-  watch7,
-  watch8,
-];
-
-export default watchArray;
---
+++ src/Components/WatchesList/WatchesList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Watch from "../Watches/watches";
+
+class WatchesList extends React.Component {
+  render() {
+    return (
+      <div className="WatchList">
+        {this.props.watches.map((watch) => <Watch watch={watch} />)}
+      </div>
+    );
+  }
+}
